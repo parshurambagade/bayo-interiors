67ab08f7a383b15b9527ced7e5e3cfc9c070068e
Parshuram Bagade <parshurambagade855@gmail.com>
2025-04-09 21:51:55 +0530

    Add: Home: Meet our client section

--- app/components/home/MeetOurClients.tsx
+++ app/components/home/MeetOurClients.tsx
@@ -1,10 +1,40 @@
 import React from "react";
 import Heading from "../Heading";
+import Image from "next/image";
 
 const MeetOurClients = () => {
   return (
-    <div className="bg-[#f6f7f8] py-10">
+    <div className="bg-[#f6f7f8] py-10 flex flex-col gap-10">
       <Heading heading="Meet Our Clients" />
+      <div className="flex justify-evenly w-full gap-8 items-center">
+        <Brand name="Brand Name" tagline="Tagline" logo="/partner-logo.svg" />
+        <Brand name="Brand Name" tagline="Tagline" logo="/partner-logo.svg" />
+        <Brand name="Brand Name" tagline="Tagline" logo="/partner-logo.svg" />
+        <Brand name="Brand Name" tagline="Tagline" logo="/partner-logo.svg" />
+        <Brand name="Brand Name" tagline="Tagline" logo="/partner-logo.svg" />
+      </div>
+    </div>
+  );
+};
+
+interface IBrand {
+  name: string;
+  tagline: string;
+  logo: string;
+}
+
+const Brand = ({ name, tagline, logo }: IBrand) => {
+  return (
+    <div className="flex flex-col gap-1 justify-center items-center w-full max-w-1/12">
+      <Image
+        src={logo}
+        height={20}
+        width={20}
+        alt={name}
+        className="w-full object-cover"
+      />
+      <p>{name}</p>
+      <p>{tagline}</p>
     </div>
   );
 };
